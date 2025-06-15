370fe7e84a22ec57aa9ff5b2ebfed6418a0fed5f
Qian-Cheng-nju <846853688@qq.com>
2025-06-15 09:57:09 +0800

    Initial commit

---
+++ run_pipeline.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This is the main script to run the entire pipeline.
+set -e
+
+echo "Pipeline starting..."
+
+# TODO: Add calls to phase-specific run scripts here.
+# Example:
+# ./phase_1_llm_generation/run.sh
+# ./phase_2_cfa_transformation/run.sh
+
+echo "Pipeline finished."
